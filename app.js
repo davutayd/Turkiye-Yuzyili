9b294fb1ed3b21ea68fc2de3bce7fb6d6d0957bb
Davut Aydemir <94298848+davutayd@users.noreply.github.com>
2023-10-25 18:59:13 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,60 @@
+function GemlikResimDegistir() {
+  var resimElementi = document.getElementById("resim");
+  resimElementi.src = "assets/togg/tr_color/smart_device_gemlik.png";
+  var resimElementi = document.getElementById("resim2");
+  resimElementi.src = "assets/togg/tr_color/gemlik.png";
+  var yeniMetin =
+    "Kampüsümüzün de yer aldığı Gemlik’in zeytin ağaçlarının gölgesindeki mavi sularından ilham alınarak tasarlandı.";
+  var pEtiketi = document.getElementById("metin");
+  pEtiketi.innerHTML = yeniMetin;
+}
+function AnadoluResimDegistir() {
+  var resimElementi = document.getElementById("resim");
+  resimElementi.src = "assets/togg/tr_color/smart_device_anadolu.png";
+  var resimElementi = document.getElementById("resim2");
+  resimElementi.src = "assets/togg/tr_color/anadolu.png";
+  var yeniMetin =
+    "Anadolu topraklarındaki cana yakınlık ve tutkuyu yansıtan kırmızıdan ilham alınarak tasarlandı. ";
+  var pEtiketi = document.getElementById("metin");
+  pEtiketi.innerHTML = yeniMetin;
+}
+function KulaResimDegistir() {
+  var resimElementi = document.getElementById("resim");
+  resimElementi.src = "assets/togg/tr_color/smart_device_kula.png";
+  var resimElementi = document.getElementById("resim2");
+  resimElementi.src = "assets/togg/tr_color/kula.png";
+  var pEtiketi = document.getElementById("metin");
+  var yeniMetin =
+    "  Kula’nın doğal yapısı, kayaçları ve vadileriyle yerkürenin milyonlarca yıllık geçmişini günümüze taşıyan gri ihtişamından ilham alınarak tasarlandı. ";
+  pEtiketi.innerHTML = yeniMetin;
+}
+function KapadokyaResimDegistir() {
+  var resimElementi = document.getElementById("resim");
+  resimElementi.src = "assets/togg/tr_color/smart_device_kapadokya.png";
+  var resimElementi = document.getElementById("resim2");
+  resimElementi.src = "assets/togg/tr_color/kapadokya.png";
+  var yeniMetin =
+    " Kapadokya’nın olağanüstü güzellikteki doğa harikası peri bacalarının toprak ve kum bejinden ilham alınarak tasarlandı. ";
+  var pEtiketi = document.getElementById("metin");
+  pEtiketi.innerHTML = yeniMetin;
+}
+function OltuResimDegistir() {
+  var resimElementi = document.getElementById("resim");
+  resimElementi.src = "assets/togg/tr_color/smart_device_oltu.png";
+  var resimElementi = document.getElementById("resim2");
+  resimElementi.src = "assets/togg/tr_color/oltu.png";
+  var yeniMetin =
+    " Oltu taşının göz alıcı siyahlığından, parlak dokusundan ve sağlam yapısından ilham alınarak tasarlandı.";
+  var pEtiketi = document.getElementById("metin");
+  pEtiketi.innerHTML = yeniMetin;
+}
+function PamukkaleResimDegistir() {
+  var resimElementi = document.getElementById("resim");
+  resimElementi.src = "assets/togg/tr_color/smart_device_pamukkale.png";
+  var resimElementi = document.getElementById("resim2");
+  resimElementi.src = "assets/togg/tr_color/pamukkale.png";
+  var yeniMetin =
+    " Pamukkale travertenlerinin göz kamaştıran beyazlığından ilham alınarak tasarlandı.";
+  var pEtiketi = document.getElementById("metin");
+  pEtiketi.innerHTML = yeniMetin;
+}
